39e4784e7e12b7e70b1031016d590a7c4b613de9
Ted Zhang <83065920+tedzhangzg@users.noreply.github.com>
2025-07-25 10:55:58 +0800

    Update script-init.sh with array of github urls

--- script-init.sh
+++ script-init.sh
@@ -18,10 +18,44 @@ echo "Starting script-init.sh ..."
 declare -a array_filenames
 declare -a array_ghlinks
 # populate
-array_filenames[0]="functions.sh"
-array_ghlinks[0]="https://raw.githubusercontent.com/tedzhangzg/scripts/main/functions.sh"
+# 
+# array
+# 
+# 0
+array_filenames[0]="script-mainmenu.sh"
+array_ghlinks[0]="https://raw.githubusercontent.com/tedzhangzg/software-install-macos/main/script-mainmenu.sh"
+# 
+# 1
 array_filenames[1]="urls.sh"
-array_ghlinks[1]="https://raw.githubusercontent.com/tedzhangzg/scripts/main/urls.sh"
+array_ghlinks[1]="https://raw.githubusercontent.com/tedzhangzg/software-install-macos/main/urls.sh"
+# 
+# 2
+array_filenames[2]="values.sh"
+array_ghlinks[2]="https://raw.githubusercontent.com/tedzhangzg/software-install-macos/main/values.sh"
+# 
+# 3
+array_filenames[3]="script-homebrew.sh"
+array_ghlinks[3]="https://raw.githubusercontent.com/tedzhangzg/software-install-macos/main/script-homebrew.sh"
+# 
+# 4
+array_filenames[4]="script-allapps.sh"
+array_ghlinks[4]="https://raw.githubusercontent.com/tedzhangzg/software-install-macos/main/script-allapps.sh"
+# 
+# 5
+array_filenames[5]="script-msoffice.sh"
+array_ghlinks[5]="https://raw.githubusercontent.com/tedzhangzg/software-install-macos/main/script-msoffice.sh"
+# 
+# 6
+array_filenames[6]="script-kms.sh"
+array_ghlinks[6]="https://raw.githubusercontent.com/tedzhangzg/software-install-macos/main/script-kms.sh"
+# 
+# 7
+array_filenames[7]="script-endusersettings.sh"
+array_ghlinks[7]="https://raw.githubusercontent.com/tedzhangzg/software-install-macos/main/script-endusersettings.sh"
+# 
+# 8
+array_filenames[8]="script-uninstall.sh"
+array_ghlinks[8]="https://raw.githubusercontent.com/tedzhangzg/software-install-macos/main/script-uninstall.sh"
 
 # loop over all key value pairs
 for i in "${!array_filenames[@]}"
